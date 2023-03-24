1d4ad7be2f8ab098ab537863c774a9c5d0eb6b43
Steve Korshakov <steve@korshakov.com>
2023-03-24 15:28:29 +0000

    feat: integration with emulator

--- src/collector/printCoverage.ts
+++ src/collector/printCoverage.ts
@@ -1,4 +1,4 @@
-import { decompileAll, opcodeToString, Printer } from "@tact-lang/opcode";
+import { decompileAll, Printer } from "@tact-lang/opcode";
 import { Cell } from "ton-core";
 import { pad, trimIndent } from "../utils/text";
 import { CoverageCollector } from "./coverage";
@@ -20,21 +20,29 @@ function template(src: string) {
                     color: #ccc;
                 }
                 .line-covered {
-                    height: 1em;
-                    background-color: #0f0;
+                    background-color: #DDFFDD;
                 }
                 .line-uncovered {
-                    height: 1em;
-                    background-color: #F6C6CE;
+                    background-color: #fffcdd;
                 }
                 .count {
-                    color: #00f;
+                    display: inline-flex;
+                    width: 30px;
+                    justify-content: center;
+                    color: #10620d;
+                }
+                .gas {
+                    display: inline-flex;
+                    width: 30px;
+                    justify-content: center;
+                    color: #e60f0f;
                 }
             </style>
         </head>
         <body>
+            <h1>Code coverage</h1>
             <pre>
-            ${pad(trimIndent(src), 8)}
+            ${pad(trimIndent(src), 16)}
             </pre>
         <body>
     </html>
@@ -46,23 +54,30 @@ export function printCoverage(code: Cell, collector: CoverageCollector) {
     // Create printer
     const printer: Printer = (src, indent) => {
         if (typeof src === 'string') {
-            return span(span('.'.repeat(indent * 2), 'padding') + src, 'line');
+            return span(span(``, 'count') + span(``, 'gas') + span('.'.repeat(indent * 2), 'padding') + src, 'line');
         }
-        let line = span('.'.repeat(indent * 2), 'padding') + span(opcodeToString(src.op), 'opcode');
+        let line = span('.'.repeat(indent * 2), 'padding') + span(src.op, 'opcode');
         let cov = collector.coverageForCell(src.hash);
         let lineClass: string;
         if (cov && cov.offsets.has(src.offset)) {
             let d = cov.offsets.get(src.offset)!!;
-            line = line + ' ' + span(`x${d.count}`, 'count');
-            let min = d.gas.reduce((a, b) => a < b ? a : b, d.gas[0]);
-            let max = d.gas.reduce((a, b) => a > b ? a : b, d.gas[0]);
-            if (min !== max) {
-                line = line + ' ' + span(`gas: ${min} - ${max}`, 'gas');
+            line = span(`x${d.count}`, 'count') + line;
+            if (src.length > 0) { // Ignore non-opcode lines
+                // let min = d.gas.reduce((a, b) => a < b ? a : b, d.gas[0]);
+                let max = d.gas.reduce((a, b) => a > b ? a : b, d.gas[0]);
+                // if (min !== max) {
+                //     line = span(`${min} - ${max}`, 'gas') + line;
+                // } else {
+                //     line = span(`${min}`, 'gas') + line;
+                // }
+                line = span(`${max}`, 'gas') + line;
             } else {
-                line = line + ' ' + span(`gas: ${min}`, 'gas');
+                line = span(``, 'gas') + line;
             }
             lineClass = 'line-covered';
         } else {
+            line = span(``, 'count') + line;
+            line = span(``, 'gas') + line;
             lineClass = 'line-uncovered';
         }
 
